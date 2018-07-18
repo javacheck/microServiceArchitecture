eba41a5eea913d565263f37d8901d9ba8c8b5e45
javacheck <261465518@139.com>
2018-07-18 21:31:57 +0800

    Initial commit

---
+++ zuul-gateway/src/main/java/cn/self/cloud/zuulgateway/ZuulGatewayApplication.java
@@ -0,0 +1,16 @@
+package cn.self.cloud.zuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulServer
+public class ZuulGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+}
